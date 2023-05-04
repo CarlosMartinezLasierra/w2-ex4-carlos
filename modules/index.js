419ccc42621da48abfb6b32b78183e910625847b
Carlos Martinez <carlosmartinezlasierra@gmail.com>
2023-05-04 13:29:45 +0200

    Testing first exercise function to get words with all vocals

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,4 @@
-const guessVocals = () => {
+const guessVocals = (word) => {
   const vocals = ["a", "e", "i", "o", "u"];
   return vocals.every((vowel) => word.includes(vowel));
 };
---
+++ modules/test.index.js
@@ -0,0 +1,6 @@
+import guessVocals from "./index";
+test("Recieve a word and counts if have all vocals", => {
+ const (word) = "murcielago"
+  expect(word).toBe(true)
+});
+
